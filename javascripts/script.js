71250d0dbd5c7d8bbe0c8be517e274bccae869af
Bernard Xie <bxie94@gmail.com>
2015-05-20 20:29:14 -0700

    done with day 1

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,25 @@
 $(window).load(function(){
+  //navbar functionality
+  //$("#active-link").focus();
 
+  //jumbotron display
+  $(".jumbotron").addClass('animated slideInLeft');
+  $(".jumbotron").css('visibility', 'visible');
+
+  //scroll-down
+  $(".scroll-down").addClass('animated pulse');
+
+  //Scrolling animation
+  function scrollTo(link, target) {
+    link.click(function(e) {
+      e.preventDefault();
+      target
+        .velocity("scroll", 1000)
+        .velocity({opacity: 1});
+    });
+  }
+
+  scrollTo($('.intro-link'), $('.introduction'));
+  scrollTo($('.resume-link'), $('.resume'));
+  scrollTo($('.projects-link'), $('.projects'));
 });
